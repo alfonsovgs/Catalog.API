1a8a67c8e5c783b645189cd0de96288a67d081fa
Alfonso <vc.alfonso@hotmail.com>
2020-02-02 13:52:46 -0600

    Clases base del dominio

---
+++ src/Catalog.Domain/Artist.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Catalog.Domain
+{
+    public class Artist
+    {
+        public Guid ArtistId { get; set; }
+        public string ArtistName { get; set; }
+    }
+}
---
+++ src/Catalog.Domain/Genre.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Catalog.Domain
+{
+    public class Genre
+    {
+        public Guid GenreId { get; set; }
+        public string GenreDescription { get; set; }
+    }
+}
