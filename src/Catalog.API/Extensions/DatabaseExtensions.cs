f670112476a7b0a46ad77e2c066c716dc973a2cb
Alfonso <vc.alfonso@hotmail.com>
2020-02-15 23:25:56 -0600

    Se crea extensión para la dependencia del dbcontext

---
+++ src/Catalog.API/Extensions/DatabaseExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Catalog.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Catalog.API.Extensions
+{
+    public static class DatabaseExtensions
+    {
+        public static void AddCatalogContext(this IServiceCollection services, string connectionString)
+        {
+            services.AddEntityFrameworkSqlServer()
+                .AddDbContext<CatalogContext>(opts =>
+                {
+                    opts.UseSqlServer(connectionString,
+                        serverOptions => { serverOptions.MigrationsAssembly(typeof(Startup).Assembly.FullName); });
+                });
+        }
+    }
+}
