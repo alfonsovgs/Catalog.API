ad3fa36efca0f1474b8fa25d6b763e7484766f9f
Alfonso <vc.alfonso@hotmail.com>
2020-05-17 22:27:17 -0500

    Definition of repositories for artist and genre models

--- src/Catalog.Domain/Repositories/IItemsRepository.cs
+++ src/Catalog.Domain/Repositories/IItemsRepository.cs
@@ -4,13 +4,12 @@ using System.Threading.Tasks;
 
 namespace Catalog.Domain.Repositories
 {
-    public interface IItemRepository : IRepository
+    public interface IItemsRepository
     {
         Task<IEnumerable<Item>> GetAsync();
         Task<Item> GetAsync(Guid id);
         Item Add(Item item);
         Item Update(Item item);
-        Task<IEnumerable<Item>> GetItemByArtistIdAsync(Guid id);
-        Task<IEnumerable<Item>> GetItemByGenreIdAsync(Guid id);
+        Item Delete(Item item);
     }
 }
