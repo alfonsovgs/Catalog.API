8ce4b2cfc9b61ee91c55f1a69d552e6125f0f792
Alfonso <vc.alfonso@hotmail.com>
2020-05-17 22:26:41 -0500

    Add services for artist and genre

---
+++ src/Catalog.Domain/Services/IGenreService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Catalog.Domain.Requests.Genre;
+using Catalog.Domain.Responses;
+
+namespace Catalog.Domain.Services
+{
+    public interface IGenreService
+    {
+        Task<IEnumerable<GenreResponse>> GetGenreAsync();
+        Task<GenreResponse> GetGenreAsync(GetGenreRequest request);
+        Task<IEnumerable<ItemResponse>> GetItemByGenreIdAsync(GetGenreRequest request);
+        Task<GenreResponse> AddGenreAsync(AddGenreRequest request);
+    }
+}
--- src/Catalog.Domain/Services/IItemService.cs
+++ src/Catalog.Domain/Services/IItemService.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using Catalog.Domain.Requests.Item;
+﻿using Catalog.Domain.Requests.Item;
 using Catalog.Domain.Responses;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
-using Catalog.Domain.Mappers;
-using Catalog.Domain.Repositories;
 
 namespace Catalog.Domain.Services
 {
@@ -15,54 +11,5 @@ namespace Catalog.Domain.Services
         Task<ItemResponse> GetItemAsync(GetItemRequest request);
         Task<ItemResponse> AddItemAsync(AddItemRequest request);
         Task<ItemResponse> EditItemAsync(EditItemRequest request);
-    }
-
-    public class ItemService : IItemService
-    {
-        private readonly IItemRepository _itemRepository;
-        private readonly IItemMapper _itemMapper;
-
-        public ItemService(IItemRepository itemRepository, IItemMapper itemMapper)
-        {
-            _itemRepository = itemRepository;
-            _itemMapper = itemMapper;
-        }
-
-        public async Task<IEnumerable<ItemResponse>> GetItemsAsync()
-        {
-            var result = await _itemRepository.GetAsync();
-            return result.Select(i => _itemMapper.Map(i));
-        }
-
-        public async Task<ItemResponse> GetItemAsync(GetItemRequest request)
-        {
-            if(request?.Id == null) throw new ArgumentNullException();
-            var entity = await _itemRepository.GetAsync(request.Id);
-
-            return _itemMapper.Map(entity);
-        }
-
-        public async Task<ItemResponse> AddItemAsync(AddItemRequest request)
-        {
-            var item = _itemMapper.Map(request);
-            var result = _itemRepository.Add(item);
-
-            await _itemRepository.UnitOfWork.SaveEntitiesAsync();
-            return _itemMapper.Map(result);
-        }
-
-        public async Task<ItemResponse> EditItemAsync(EditItemRequest request)
-        {
-            var existingRecord = await _itemRepository.GetAsync(request.Id);
-
-            if (existingRecord == null)
-            {
-                throw new ArgumentException($"Entity with {request.Id} is not present");
-            }
-
-            var entity = _itemMapper.Map(request);
-            var result = _itemRepository.Update(entity);
-            return _itemMapper.Map(result);
-        }
     }
 }
