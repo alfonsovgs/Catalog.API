94e7eafe9ab4cd46d80ec5ea15dbd8b18ca2dce0
Alfonso <vc.alfonso@hotmail.com>
2020-08-13 12:53:42 -0500

    domain entities created

--- Cart/src/Cart.Domain/Entities/CartItem.cs
+++ Cart/src/Cart.Domain/Entities/CartItem.cs
@@ -4,7 +4,7 @@ namespace Cart.Domain.Entities
 {
     public class CartItem
     {
-        public Guid CartItemid { get; set; }
+        public Guid CartItemId { get; set; }
         public int Quantity { get; set; }
 
         public void IncreaseQuantity()
