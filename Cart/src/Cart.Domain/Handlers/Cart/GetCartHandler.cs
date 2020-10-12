23a3e81f787d9060344b5df8cbbb53fa29ec9666
Alfonso <vc.alfonso@hotmail.com>
2020-10-12 11:23:07 -0500

    Handler to get cart information

---
+++ Cart/src/Cart.Domain/Handlers/Cart/GetCartHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using Cart.Domain.Commands.Cart;
+using Cart.Domain.Repositories;
+using Cart.Domain.Responses.Cart;
+using Cart.Domain.Services;
+using MediatR;
+
+namespace Cart.Domain.Handlers.Cart
+{
+    public class GetCartHandler : IRequestHandler<GetCartCommand, CartExtendedResponse>
+    {
+        private readonly ICartRepository _repository;
+        private readonly IMapper _mapper;
+        private readonly ICatalogService _catalogService;
+
+        public GetCartHandler(ICartRepository repository, IMapper mapper, ICatalogService catalogService)
+        {
+            _repository = repository;
+            _mapper = mapper;
+            _catalogService = catalogService;
+        }
+
+        public async Task<CartExtendedResponse> Handle(GetCartCommand request, CancellationToken cancellationToken)
+        {
+            var result = await _repository.GetAsync(request.Id);
+            var extendedResponse = _mapper.Map<CartExtendedResponse>(result);
+
+            var tasks = extendedResponse.Items
+                .Select(async item => await _catalogService.EnrichCartItem(item, cancellationToken));
+
+            extendedResponse.Items = await Task.WhenAll(tasks);
+            return extendedResponse;
+        }
+    }
+}
