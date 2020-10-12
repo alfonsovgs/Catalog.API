335e2f0d0f8cd8a278c13103077e77f9406f15c2
Alfonso <vc.alfonso@hotmail.com>
2020-10-12 11:25:57 -0500

    Policy implemented.

---
+++ Cart/src/Cart.Infrastructure/Extensions/CatalogServiceExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Cart.Domain.Services;
+using Cart.Infrastructure.Extensions.Policies;
+using Cart.Infrastructure.Services;
+using Catalog.API.Client;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Cart.Infrastructure
+{
+    public static class CatalogServiceExtensions
+    {
+        public static IServiceCollection AddCatalogService(this IServiceCollection services, Uri uri)
+        {
+            services.AddScoped<ICatalogService, CatalogService>();
+            services.AddHttpClient<ICatalogClient, CatalogClient>(client => client.BaseAddress = uri)
+                .SetHandlerLifetime(TimeSpan.FromMinutes(2))
+                .AddPolicyHandler(CatalogServicePolicies.RetryPolicy())
+                .AddPolicyHandler(CatalogServicePolicies.CircuitBreakerPolicy());
+
+
+            return services;
+        }
+    }
+}
---
+++ Cart/src/Cart.Infrastructure/Extensions/Policies/CatalogServicePolicies.cs
@@ -0,0 +1,26 @@
+﻿using Polly;
+using Polly.Extensions.Http;
+using System;
+using System.Net;
+using System.Net.Http;
+
+namespace Cart.Infrastructure.Extensions.Policies
+{
+    public static class CatalogServicePolicies
+    {
+        public static IAsyncPolicy<HttpResponseMessage> RetryPolicy()
+        {
+            return HttpPolicyExtensions
+                .HandleTransientHttpError()
+                .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+                .WaitAndRetryAsync(3, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
+        }
+
+        public static IAsyncPolicy<HttpResponseMessage> CircuitBreakerPolicy()
+        {
+            return HttpPolicyExtensions
+                .HandleTransientHttpError()
+                .CircuitBreakerAsync(3, TimeSpan.FromMinutes(1));
+        }
+    }
+}
