c7d235839ff517a9c719433075557c74753d399e
Alfonso <vc.alfonso@hotmail.com>
2020-10-12 12:12:22 -0500

    Cart controller implemented

---
+++ Cart/src/Cart.API/Controllers/CartController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading.Tasks;
+using Cart.Domain.Commands.Cart;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cart.API.Controllers
+{
+    public class CartController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public CartController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            var result = await _mediator.Send(new GetCartCommand { Id = id });
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(CreateCartCommand request)
+        {
+            var result = await _mediator.Send(request);
+            return CreatedAtAction(nameof(GetById), new { id = result.Id }, null);
+        }
+
+        [HttpPut("{cartId:guid}/items/{id:guid}")]
+        public async Task<IActionResult> Put(Guid cartid, Guid id)
+        {
+            var result = await _mediator.Send(new UpdateCartItemQuantityCommand
+            {
+                CartId = cartid,
+                CartItemId = id,
+                IsAddOperation = true,
+            });
+
+            return Ok(result);
+        }
+
+        [HttpDelete("{cartId:guid}/items/{id:guid}")]
+        public async Task<IActionResult> Delete(Guid cartId, Guid id)
+        {
+            var result = await _mediator.Send(new UpdateCartItemQuantityCommand
+            {
+                CartId = cartId,
+                CartItemId = id,
+                IsAddOperation = false
+            });
+
+            return Ok(result);
+        }
+
+    }
+}
