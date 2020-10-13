d888b034dd22cd86f1dbde349bfc35acb15e2f1c
Alfonso <vc.alfonso@hotmail.com>
2020-10-13 11:08:37 -0500

    Controller cart modified

--- Cart/src/Cart.API/Controllers/CartController.cs
+++ Cart/src/Cart.API/Controllers/CartController.cs
@@ -1,11 +1,15 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using Cart.API.Infrastructure.Filters;
 using Cart.Domain.Commands.Cart;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
 
 namespace Cart.API.Controllers
 {
+    [Route("api/cart")]
+    [ApiController]
+    [JsonException]
     public class CartController : ControllerBase
     {
         private readonly IMediator _mediator;
