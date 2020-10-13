ab28629ddec6c492d538cbc3c12f1424586679b8
Alfonso <vc.alfonso@hotmail.com>
2020-10-13 11:06:02 -0500

    event bus injected

--- Catalog/src/Catalog.Domain/Services/ItemService.cs
+++ Catalog/src/Catalog.Domain/Services/ItemService.cs
@@ -1,11 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Catalog.Domain.Configurations;
 using Catalog.Domain.Mappers;
 using Catalog.Domain.Repositories;
 using Catalog.Domain.Requests.Item;
 using Catalog.Domain.Responses;
+using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Catalog.Domain.Events;
+using System.Text.Json;
 
 namespace Catalog.Domain.Services
 {
@@ -13,11 +19,20 @@ namespace Catalog.Domain.Services
     {
         private readonly IItemRepository _itemRepository;
         private readonly IItemMapper _itemMapper;
+        private readonly ConnectionFactory _eventBusConnectionFactory;
+        private readonly ILogger<ItemService> _logger;
+        private readonly EventBusSettings _settings;
 
-        public ItemService(IItemRepository itemRepository, IItemMapper itemMapper)
+        public ItemService(IItemRepository itemRepository, IItemMapper itemMapper,
+            ConnectionFactory eventBusConnectionFactory,
+            ILogger<ItemService> logger,
+            EventBusSettings settings)
         {
             _itemRepository = itemRepository;
             _itemMapper = itemMapper;
+            _eventBusConnectionFactory = eventBusConnectionFactory;
+            _logger = logger;
+            _settings = settings;
         }
 
         public async Task<IEnumerable<ItemResponse>> GetItemsAsync()
@@ -28,7 +43,7 @@ namespace Catalog.Domain.Services
 
         public async Task<ItemResponse> GetItemAsync(GetItemRequest request)
         {
-            if(request?.Id == null) throw new ArgumentNullException();
+            if (request?.Id == null) throw new ArgumentNullException();
             var entity = await _itemRepository.GetAsync(request.Id);
 
             return _itemMapper.Map(entity);
@@ -59,7 +74,7 @@ namespace Catalog.Domain.Services
 
         public async Task<ItemResponse> DeleteItemAsync(DeleteItemRequest request)
         {
-            if(request?.Id == null) throw new ArgumentNullException();
+            if (request?.Id == null) throw new ArgumentNullException();
 
             var result = await _itemRepository.GetAsync(request.Id);
             result.IsInactive = true;
@@ -67,7 +82,29 @@ namespace Catalog.Domain.Services
             _itemRepository.Update(result);
             await _itemRepository.UnitOfWork.SaveChangesAsync();
 
+            SendDeleteMessage(new ItemSoldOutEvent { Id = request.Id.ToString() });
             return _itemMapper.Map(result);
+        }
+
+        private void SendDeleteMessage(ItemSoldOutEvent message)
+        {
+            try
+            {
+                var connection = _eventBusConnectionFactory.CreateConnection();
+
+                using var channel = connection.CreateModel();
+                channel.QueueDeclare(queue: _settings.EventQueue, true, false);
+
+                var body = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(message));
+
+                channel.ConfirmSelect();
+                channel.BasicPublish(exchange: string.Empty, routingKey: _settings.EventQueue, body: body);
+                channel.WaitForConfirmsOrDie();
+            }
+            catch (Exception e)
+            {
+                _logger.LogWarning("Unable to initialize the event bus: {message}", e.Message);
+            }
         }
     }
 }
