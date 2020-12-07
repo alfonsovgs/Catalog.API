25658e36eb7e6d1f3c914ecdcc456570482f08be
Alfonso <vc.alfonso@hotmail.com>
2020-12-07 08:46:29 -0600

    UserRepository implemented

---
+++ Catalog/src/Catalog.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,41 @@
+﻿using Catalog.Domain.Repositories;
+using System.Threading;
+using System.Threading.Tasks;
+using Catalog.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalog.Infrastructure.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public UserRepository(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public async Task<bool> AuthenticateAsync(string email, string password, CancellationToken cancellationToken = default)
+        {
+            var result = await _signInManager.PasswordSignInAsync(email, password, false, false);
+            return result.Succeeded;
+
+        }
+
+        public async Task<bool> SignUpAsync(User user, string password, CancellationToken cancellationToken = default)
+        {
+            var result = await _userManager.CreateAsync(user, password);
+            return result.Succeeded;
+        }
+
+        public async Task<User> GetByEmailAsync(string requestEmail, CancellationToken cancellationToken = default)
+        {
+            return await _userManager
+                .Users
+                .FirstOrDefaultAsync(u => u.Email == requestEmail, cancellationToken);
+        }
+    }
+}
