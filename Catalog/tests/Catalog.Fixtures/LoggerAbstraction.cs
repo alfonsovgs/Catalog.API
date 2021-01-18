6eb6c44aeadfac0b4c168df440c3e2f4b0ad6bd2
Alfonso <vc.alfonso@hotmail.com>
2021-01-18 09:57:24 -0600

    LoggerAbstraction created

---
+++ Catalog/tests/Catalog.Fixtures/LoggerAbstraction.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Catalog.Fixtures
+{
+    public abstract class LoggerAbstraction<T> : ILogger<T>
+    {
+        public IDisposable BeginScope<TState>(TState state) => throw new NotImplementedException();
+
+        public bool IsEnabled(LogLevel logLevel) => throw new NotImplementedException();
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception,
+            Func<TState, Exception, string> formatter) => throw new NotImplementedException();
+
+        public abstract void Log(LogLevel logLevel, Exception ex, string information);
+    }
+}
